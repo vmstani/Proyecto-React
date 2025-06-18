c21ea7d7117e091c2490f8d0015747a74c5cffec
vstanislavsky <vstanislavsky@inti.gob.ar>
2025-06-18 14:15:47 -0300

    redes sociales

--- src/components/estatics/Footer.jsx
+++ src/components/estatics/Footer.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import './styleEstatico.css'
-
+import React from 'react';
+import './styleStatic.css';
 
 const Footer = () => {
   return (
     <footer>
-        <p>&copy; 2025 </p>
+      <p>&copy; Todos los derechos reservados</p>
+      <div className="social-icons">
+        <a href="https://www.facebook.com/vero.trainer" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://x.com/VeroStanis" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com/verotrainer" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/estatics/Header.jsx
+++ src/components/estatics/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './styleEstatico.css';
+import './styleStatic.css';
 import Cart from '../Cart';
 import logo from '../../assets/logo.png';
 import { useCart } from '../../context/CartContext'; 
